33984eb86c155805a947aac8fc142112fa957444
深情小建 <374452668@qq.com>
2019-06-02 23:21:58 +0800

    事件添加

--- 54Helper/index.js
+++ 54Helper/index.js
@@ -19,8 +19,8 @@ $(function() {
 		},
 		"types": {
 			"#": {
-				"max_children": 100,
-				"max_depth": 400,
+				"max_children": -1, // 最大直接子节点数
+				"max_depth": -1, // 最大嵌套数
 				"valid_children": ["root"]
 			},
 			"root": {
@@ -36,7 +36,58 @@ $(function() {
 			}
 		},
 		"plugins": [
-			"contextmenu", "dnd", "search", "state", "types", "wholerow"
+			// "checkbox",// 三态复选框
+			"contextmenu", //右键菜单 
+			"dnd", // 拖拽
+			// "massload",// 此插件可以在单个请求中加载节点（与延迟加载一起使用）
+			"search", //搜索
+			// "sort",// 排序兄弟节点
+			"state", //此插件将所有打开和选定的节点保存在用户的浏览器中，因此当返回到同一个树时，将恢复先前的状态。
+			"types", //节点组添加预定义模式
+			"unique", // 同级节点不可相同
+			// "wholerow",// 使每个节点显示块级别，使选择更容易
+			"changed",
+			"conditionalselect"
 		]
+	});
+
+	// jstree搜索
+	var to = false;
+	$('#jstree_search').keyup(function() {
+		if(to) {
+			clearTimeout(to);
+		}
+		to = setTimeout(function() {
+			var v = $('#jstree_search').val();
+			$('#jstree').jstree(true).search(v);
+		}, 250);
+	});
+
+	// jstree节点点击事件处理
+	$('#jstree').on("changed.jstree", function(e, data) {
+		if(data.action !== "select_node") {
+			// 节点选择事件触发(左键点击、右键点击)
+			console.log("data.action: " + data.action);
+			return;
+		}
+		if(!data.event) {
+			// TODO 这里处理首次加载逻辑
+			console.log("首次加载页面: " + data.node.text);
+			return;
+		}
+		if(!data.node) {
+			console.log("data.node：" + data.node);
+			return;
+		}
+		if(data.event.type !== "click") {
+			console.log("data.event.type：" + data.event.type);
+			return;
+		}
+		if(data.node.parent === "#") {
+			alert("根节点：" + data.node.text);
+		} else {
+			alert("子节点：" + data.node.text);
+		}
+		console.log(data.selected);
 	});
 });
